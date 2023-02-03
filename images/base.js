c412a9d4c1ce4852a739e731551abe89c2f2e81c
active-webmaker <111613055+active-webmaker@users.noreply.github.com>
2023-02-03 20:20:16 +0900

    base.js update 230203
    base.js update 230203

--- images/base.js
+++ images/base.js
@@ -18,10 +18,11 @@ function Dload(){
     for(con of con_li) {
       ftr_res1.add(con[1]);
     }
-
+    
     // 데이터 제목 내용 HTML 상에 반영 & 타이틀 이미지 변경
     $("#title-part span").html(ttl);
-    $("#title-part img").attr("src" ,`./images/light_150.png`);
+    $("#org-img").addClass("hidden");
+    $("#chg-img").removeClass("hidden");
 
     // HTML 상에 1차 필터 메뉴 반영하기
     let ftr_con1 = '<option></option>\n<option>전체</option>\n<option>'
