96a4f7f8d195e01f5cb440a15e90c50edeeed007
active-webmaker <111613055+active-webmaker@users.noreply.github.com>
2023-02-03 20:22:58 +0900

    csv-parser.js update
    csv-parser.js update

--- images/csv-parser.js
+++ images/csv-parser.js
@@ -1,31 +1,4 @@
 function CsvSplit(csv_file) {
-  let wnum = 0;
-  let bslc = "";
-  let bstart = 0;
-  let bend = csv_file.length - 1;
-  let bnum = 0;
-  let block_li = [];
-
-  bnum = csv_file.indexOf(`"`);
-  while (bnum != -1) {
-    bstart = csv_file.indexOf(`"`);
-    if (bstart == -1) {
-      break
-    }
-    bend = csv_file.slice(bstart+1,csv_file.length).indexOf(`"`);
-    if (bend != -1){
-      bend += bstart+1
-      bslc =  csv_file.slice(bstart, bend+1)
-      block_li.push(bslc);
-      csv_file = csv_file.replace(bslc, `{% String-${wnum} %}`)
-      wnum++;
-      bstart = bend+1;
-    }
-    else {
-      break
-    }
-  }
-
   let idx_li = [`\r\n`, `\r`, `\n`];
   let idx_str = "";
   let inum;
@@ -37,6 +10,96 @@ function CsvSplit(csv_file) {
       break
     }
   }
+
+  let bstart = 0;
+  let bend = csv_file.length - 1;
+  let bnum = 0;
+
+  // 큰따옴표 1개
+
+  let wnum = 0;
+  let bslc = "";
+  let cbslc = "";
+  let block_li = [];
+  bstart = 0;
+  bend = csv_file.length;
+  bnum = csv_file.indexOf(`"`);
+
+  while (bnum != -1) {
+    let xwnum = 0;
+    bstart = csv_file.indexOf(`"`);
+    if (bstart == -1) {
+      break
+    }
+    if (csv_file.slice(bstart,bstart+2) == `""`) {
+      if (csv_file[bstart+2] == `"`) {
+        let xnum = 1;
+        while (xnum != -1) {
+          if (csv_file[bstart+2+xnum] == `"`) {
+            xnum++
+          }
+          else {
+            if (xnum % 2 == 0) {
+              csv_file = csv_file.slice(0,bstart)
+                + csv_file.slice(bstart, bstart+2+xnum).replaceAll(`""`,`&#34;`)
+                + csv_file.slice(bstart+2+xnum);
+              xwnum = -1
+              break
+            }
+            else {
+              csv_file = csv_file.slice(0,bstart+1)
+                + csv_file.slice(bstart+1, bstart+2+xnum).replaceAll(`""`,`&#34;`)
+                + csv_file.slice(bstart+2+xnum);
+              xwnum = -1
+              break
+            }
+          }
+        }
+      }
+      else {
+        csv_file = csv_file.slice(0,bstart)
+          + `&#34;` + csv_file.slice(bstart+2);
+        xwnum = -1
+      }
+    }
+    if (xwnum == -1) {
+      continue
+    }
+
+    slc_num = bstart+1;
+    bend = csv_file.slice(slc_num).indexOf(`"`);
+
+    while (bend != -1) {
+      if (slc_num > csv_file.length) {
+        break
+      }
+      bend = csv_file.slice(slc_num).indexOf(`"`);
+      if (bend != -1) {
+        bend += slc_num
+        if (csv_file[bend+1] == `"`) {
+          csv_file = csv_file.slice(0,bend) + `&#34;`
+            + csv_file.slice(bend+2);
+          slc_num = bend+5;
+        }
+        else {
+          bslc = csv_file.slice(bstart, bend+1)
+          cbslc = bslc.slice(1, bslc.length-1)
+          for (i of idx_li) {
+            cbslc = cbslc.replaceAll(i, "<p></p>")
+          }
+          block_li[wnum] = cbslc;
+            csv_file = csv_file.replace(bslc, `{% String-${wnum} %}`)
+          wnum++;
+          break
+        }
+      }
+      else {
+        break
+      }
+    }
+  }
+
+
   if (idx_str == "") {
     result = [csv_file,];
   }
@@ -49,7 +112,7 @@ function CsvSplit(csv_file) {
       result.splice(i,1);
     }
   }
-  
+
   for (var i=0; i < result.length; i++) {
     result[i] = result[i].split(",");
   }
